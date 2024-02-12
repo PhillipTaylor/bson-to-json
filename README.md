c50104b7d75d3bc919a58585f28a828adb4a8926
Phillip Taylor <phillip.taylor@transformuk.com>
2024-02-12 22:07:19 +0000

    Add a --pandas command

--- README.md
+++ README.md
@@ -14,7 +14,39 @@ java -jar bson-to-json.jar mycollection.bson.gz output.json
 
 ## Design
 
-This application is written in Scala and uses the fs2 streaming library. It's streaming architecture, that includes inline gzip decompression can work with files 10GB+ with minimal ram requirements. If you have a slow machine you can try and tune the JVM `export JAVA_OPTS="-XX:+UseG1GC -Xmx4096m"`.
+This application is written in Scala and uses the fs2 streaming library. It's streaming architecture, that includes inline gzip decompression can work with files 10GB+ with minimal ram requirements. 
+
+## Options
+
+If you have a slow machine you can try and tune the JVM `export JAVA_OPTS="-XX:+UseG1GC -Xmx4096m"`.
+
+### Pandas Mode
+
+Normally when converting bson objects to json, we define the collection inside an array like this.
+
+```json
+[
+   {
+    "id" : 1,
+    "name": "phill"
+   },
+   {
+    "id" : 2,
+    "name": "loretta"
+   }
+]
+```
+
+Since json.loads tries to load the whole array it can make your system run out of memory. Pandas.read_json also has the same problem. To overcome this Pandas.read_json has a `lines=True` mode.
+
+You can pass a `--pandas` argument into this app to make it skip the outer array and to use newlines between each json object, so the output is like this:
+
+```json
+{ "id" : 1, "name": "phill" }
+{ "id" : 2, "name": "loretta" }
+```
+
+This is also helpful when loading the data into elasticsearch as well.
 
 ## Deployment
 
--- src/main/scala/uk/gov/homeoffice/bsonreader/Main.scala
+++ src/main/scala/uk/gov/homeoffice/bsonreader/Main.scala
@@ -47,20 +47,81 @@ object MainApp extends IOApp:
       .collect { case Right(json) => json }
   }
 
+  def writeNormalJson(fos: FileOutputStream, json :Json) :Unit = {
+    fos.write(json.spaces4.getBytes)
+    fos.write(",\n".getBytes)
+  }
+
+  def writePandasJson(fos :FileOutputStream, json :Json) :Unit = {
+    fos.write(json.noSpaces.getBytes)
+    fos.write("\n".getBytes)
+  }
+
+  // stolen from https://stackoverflow.com/questions/58026172/flattening-nested-json-objects-with-circe
+  def jsonFlatten(json :Json) :Json = {
+    def flattenDeep(combineKeys: (String, String) => String)(value: Json): Json = {
+      def flattenToFields(value: Json): Option[Iterable[(String, Json)]] = {
+        value.fold(
+          jsonNull = None,
+          jsonNumber = _ => None,
+          jsonString = _ => None,
+          jsonBoolean = _ => None,
+          jsonObject = { obj =>
+            val fields = obj.toIterable.flatMap {
+              case (field, json) =>
+                flattenToFields(json).fold(Iterable(field -> json)) {
+                  _.map {
+                    case (innerField, innerJson) =>
+                      combineKeys(field, innerField) -> innerJson
+                  }
+                }
+            }
+            Some(fields)
+          },
+          jsonArray = { array =>
+            val fields = array.zipWithIndex.flatMap {
+              case (json, index) =>
+                flattenToFields(json).fold(Iterable(index.toString -> json)) {
+                  _.map {
+                    case (innerField, innerJson) =>
+                      combineKeys(index.toString, innerField) -> innerJson
+                  }
+                }
+            }
+            Some(fields)
+          }
+        )
+      }
+      flattenToFields(value).fold(value)(Json.fromFields)
+    }
+
+    def dotNotation(a :String, b :String) = s"$a.$b"
+    flattenDeep(dotNotation)(json)
+  }
+
+  def noOpFlatten(json :Json) :Json = json
+
   def run(args: List[String]): IO[ExitCode] = {
     val inputFile = args(0)
     val outputFile = args(1)
+    val pandasMode = args.contains("--pandas")
+
     val fos = new FileOutputStream(outputFile)
-    fos.write("[\n".getBytes)
+
+    if (pandasMode) { fos.write("[\n".getBytes) }
+
+    val stringifier = if (pandasMode) writePandasJson else writeNormalJson
+    val flattener = if (pandasMode) jsonFlatten else noOpFlatten
+
     bsonReader(inputFile)
-      .map { json =>
-        fos.write(json.spaces4.getBytes)
-        fos.write(",\n".getBytes)
-      }
+      .zipWithIndex
+      .evalTap { (_, idx) => IO(if (idx % 100000 == 0) { println(s"Written $idx") }) }
+      .map { (json, _) => flattener(json) }
+      .map { json => stringifier(fos, json) }
       .compile
       .drain
       .map { _ =>
-        fos.write("]".getBytes)
+        if (!pandasMode) { fos.write("]".getBytes) }
         fos.close()
       }
       .as(ExitCode(0))
